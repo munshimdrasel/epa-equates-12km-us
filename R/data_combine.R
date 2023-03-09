ebbd8b433d66986aeeaf4cdb13ca636608db27a8
munshimdrasel <munshimdrasel@gwu.edu>
2023-03-09 09:59:34 -0500

    data clean and combine

--- R/data_combine.R
+++ R/data_combine.R
@@ -1,19 +1,29 @@
 rm(list = ls())
 
-library(tidyverse)
-library(geosphere) #Needed for Haversine distance
-library(readxl)
-library(rgeos)
-library(zoo)
-library(fst)
-library(data.table)
-library(ggplot2)
-library(stringi)
+
+library( data.table)
+library( magrittr)
+library( raster)
+library( sf)
+library( ggplot2)
+require(readxl)
+require(tidyverse)
 library(lubridate)
-library(dslabs)
-library(stringi)
-library(gridExtra)
-library(ggmap)
-library(Rmisc)
-library(usmap)
+
+#on my laptop
+setwd ("/Users/munshirasel/Library/CloudStorage/GoogleDrive-munshimdrasel@gwmail.gwu.edu/My Drive/R/epa-equates-12km-us")
+
+#on Hopper
+# setwd ("/Users/munshirasel/Library/CloudStorage/GoogleDrive-munshimdrasel@gwmail.gwu.edu/My Drive/R/epa-equates-12km-us")
+
+data_dir <- './data/'
+
+data.files = list.files(data_dir, pattern = "*.csv")
+read_csv_files <- function(x){
+  df <- read.csv(path = paste(data_dir, x, sep = "/"))
+  return(df)
+}
+
+df <- lapply(data.files, read_xlsx_files ) %>%
+  bind_rows()
 
